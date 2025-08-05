fd2155b27fae96cd06cb0edf77635ce1766f6d39
sharif <msharif.ahrari@gmail.com>
2025-08-05 09:53:31 +0330

    Initial commit for rmscms/telegram package

---
+++ src/TelegramServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RMS\Telegram;
+
+
+
+use Illuminate\Support\ServiceProvider;
+
+class TelegramServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register(){
+
+    }
+}
